38becf486852dd4ee6baa3a51becf0b354bbf882
tomas <72792707+tomas-land@users.noreply.github.com>
2023-05-16 23:14:53 +0300

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,21 @@
-//External Lib
 
-//Internal Lib
-import { AppProvider } from '@context/context';
-//Styles
-import "../styles/globals.scss"
 
-export default function RootLayout({ children }: { children: any }) {
-  return (
+export default function RootLayout({
+  children,
+}: {
+  children: any
+}) {  return (
     <html>
       <body>
         <main>
           <div className="wrapper">
-            <AppProvider>
+
               {children}
-            </AppProvider>
+      
           </div>
         </main>
       </body>
     </html >
   )
 }
+
