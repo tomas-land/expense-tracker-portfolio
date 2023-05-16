c521d620078a59490bf29defa16c5eed830e2bd9
tomas <72792707+tomas-land@users.noreply.github.com>
2023-05-16 23:45:17 +0300

    init

--- app/statistics/page.tsx
+++ app/statistics/page.tsx
@@ -1,29 +1,29 @@
 
-//Components
-import TotalExpensesList from './TotalExpensesList';
-import TotalDisplay from './TotalDisplay';
-import Chart from './Chart';
-import TopButtons from './TopButtons';
-//Internal Lib
-import { getTotalAmountExpenses, getPreviousMonthTotalAmountExpenses } from '@lib/prisma/expenses'
-import { getCategoriesWithExpenses } from '@lib/prisma/expenses_by_category'
-//Styles
-import s from '@styles/Pages/_StatsPage.module.scss'
+// //Components
+// import TotalExpensesList from './TotalExpensesList';
+// import TotalDisplay from './TotalDisplay';
+// import Chart from './Chart';
+// import TopButtons from './TopButtons';
+// //Internal Lib
+// import { getTotalAmountExpenses, getPreviousMonthTotalAmountExpenses } from '@lib/prisma/expenses'
+// import { getCategoriesWithExpenses } from '@lib/prisma/expenses_by_category'
+// //Styles
+// import s from '@styles/Pages/_StatsPage.module.scss'
 
-export const dynamic = 'force-dynamic'
+// export const dynamic = 'force-dynamic'
 
 
 const StatsPage = async () => {
-  const categoriesWithExpenses = await getCategoriesWithExpenses();
-  const totalAmountExpenses = await getTotalAmountExpenses();
-  const previousMonthTotalAmountExpenses = await getPreviousMonthTotalAmountExpenses();
+  // const categoriesWithExpenses = await getCategoriesWithExpenses();
+  // const totalAmountExpenses = await getTotalAmountExpenses();
+  // const previousMonthTotalAmountExpenses = await getPreviousMonthTotalAmountExpenses();
 
   return (
-    <section className={s.stats_page}>
-      <TopButtons/>
+    <section>
+      {/* <TopButtons/>
       <TotalDisplay totalAmountExpenses={totalAmountExpenses} previousMonthTotalAmountExpenses={previousMonthTotalAmountExpenses} />
       <Chart categoriesWithExpenses={categoriesWithExpenses} />
-      <TotalExpensesList categoriesWithExpenses={categoriesWithExpenses} />
+      <TotalExpensesList categoriesWithExpenses={categoriesWithExpenses} /> */}
     </section>
   )
 }
